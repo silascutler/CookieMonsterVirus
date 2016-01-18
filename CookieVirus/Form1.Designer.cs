e2b1e13750b64de369e342ba221eab3baecc838e
Silas <Silas.Cutler@Gmail.com>
2016-01-17 21:20:56 -0500

    Init

---
+++ CookieVirus/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace CookieVirus
+{
+    partial class CookieVirus
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.starttimer = new System.Windows.Forms.Timer(this.components);
+            this.movetimer = new System.Windows.Forms.Timer(this.components);
+            this.intro0 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.monster0 = new System.Windows.Forms.PictureBox();
+            this.cookie1 = new System.Windows.Forms.PictureBox();
+            this.cookie0 = new System.Windows.Forms.PictureBox();
+            this.intro1 = new System.Windows.Forms.PictureBox();
+            this.chomp = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.monster0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cookie1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cookie0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.intro1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // intro0
+            // 
+            this.intro0.AutoSize = true;
+            this.intro0.Font = new System.Drawing.Font("Segoe UI Symbol", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.intro0.Location = new System.Drawing.Point(633, 39);
+            this.intro0.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.intro0.Name = "intro0";
+            this.intro0.Size = new System.Drawing.Size(798, 384);
+            this.intro0.TabIndex = 0;
+            this.intro0.Text = "I want a Cookie.\r\nGive me a cookie.\r\nN     W!";
+            this.intro0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1924, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // monster0
+            // 
+            this.monster0.Image = global::CookieVirus.Properties.Resources._9cpbzoRyi;
+            this.monster0.Location = new System.Drawing.Point(12, 61);
+            this.monster0.Name = "monster0";
+            this.monster0.Size = new System.Drawing.Size(209, 102);
+            this.monster0.TabIndex = 6;
+            this.monster0.TabStop = false;
+            this.monster0.Visible = false;
+            // 
+            // cookie1
+            // 
+            this.cookie1.Image = global::CookieVirus.Properties.Resources.tumblr_inline_nlivuq0i4t1qmxivf_400;
+            this.cookie1.Location = new System.Drawing.Point(423, 52);
+            this.cookie1.Name = "cookie1";
+            this.cookie1.Size = new System.Drawing.Size(121, 111);
+            this.cookie1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.cookie1.TabIndex = 4;
+            this.cookie1.TabStop = false;
+            this.cookie1.Visible = false;
+            // 
+            // cookie0
+            // 
+            this.cookie0.Image = global::CookieVirus.Properties.Resources.tumblr_inline_nlivuq0i4t1qmxivf_400;
+            this.cookie0.Location = new System.Drawing.Point(261, 52);
+            this.cookie0.Name = "cookie0";
+            this.cookie0.Size = new System.Drawing.Size(121, 111);
+            this.cookie0.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.cookie0.TabIndex = 3;
+            this.cookie0.TabStop = false;
+            this.cookie0.Visible = false;
+            // 
+            // intro1
+            // 
+            this.intro1.Image = global::CookieVirus.Properties.Resources.cookie_monster_clip_art_cookie_monster_by_neorame_d4yb0b5;
+            this.intro1.Location = new System.Drawing.Point(945, 300);
+            this.intro1.Name = "intro1";
+            this.intro1.Size = new System.Drawing.Size(123, 123);
+            this.intro1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.intro1.TabIndex = 1;
+            this.intro1.TabStop = false;
+            // 
+            // chomp
+            // 
+            this.chomp.AutoSize = true;
+            this.chomp.Location = new System.Drawing.Point(220, 193);
+            this.chomp.Name = "chomp";
+            this.chomp.Size = new System.Drawing.Size(144, 47);
+            this.chomp.TabIndex = 7;
+            this.chomp.Text = "Chomp!";
+            this.chomp.Visible = false;
+            // 
+            // CookieVirus
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 47F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(1924, 942);
+            this.Controls.Add(this.chomp);
+            this.Controls.Add(this.monster0);
+            this.Controls.Add(this.cookie1);
+            this.Controls.Add(this.cookie0);
+            this.Controls.Add(this.intro1);
+            this.Controls.Add(this.intro0);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Segoe UI Symbol", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(10, 11, 10, 11);
+            this.Name = "CookieVirus";
+            this.Opacity = 0.75D;
+            this.Text = "CookieVirus";
+            this.Load += new System.EventHandler(this.CookieVirus_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.monster0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cookie1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cookie0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.intro1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer starttimer;
+        private System.Windows.Forms.Timer movetimer;
+        private System.Windows.Forms.Label intro0;
+        private System.Windows.Forms.PictureBox intro1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.PictureBox cookie0;
+        private System.Windows.Forms.PictureBox cookie1;
+        private System.Windows.Forms.PictureBox monster0;
+        private System.Windows.Forms.Label chomp;
+    }
+}
+
---
+++ CookieVirus/Form1.cs
@@ -0,0 +1,108 @@
+﻿/*
+ * Created by Silas Cutler <P1nk> 2016
+ * Shouts to ZeroCool, AcidBurn, Lork Nikon, The King of Ninex, Dr Doom, Joey, Razer, Blade, Cereal, Phreak
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CookieVirus
+{
+    public partial class CookieVirus : Form
+    {
+        public CookieVirus()
+        {
+            InitializeComponent();
+            this.Bounds = Bounds;
+        }
+
+        private void CookieVirus_Load(object sender, EventArgs e)
+        {
+            Cursor.Hide();
+            TopMost = true;
+
+            monster0.Visible = false;
+            cookie0.Visible = false;
+            cookie1.Visible = false;
+            chomp.Visible = false;
+
+            starttimer.Interval = 1500;
+            starttimer.Tick +=new EventHandler(starttimer_Tick);
+            starttimer.Start();
+
+            movetimer.Interval = 1;
+            movetimer.Tick +=new EventHandler(movetimer_Tick);
+            movetimer.Start();
+
+        }
+
+        private int counter = 0;
+
+
+        private void movetimer_Tick(object sender, System.EventArgs e)
+        {
+            monster0.Left = monster0.Left + 10;
+            if (counter <= 0)
+                chomp.Visible = false;
+            else
+                counter = counter - 1;
+
+
+            if (monster0.Right >= cookie0.Left)
+            {
+                // Move Cookie 0 Past Cookie 1
+                cookie0.Left = cookie1.Right + 50;
+                counter = 10;
+            }
+            if (monster0.Right >= cookie1.Left)
+            {
+                // Move Cookie 1 Past Cookie 0
+                cookie1.Left = cookie0.Right + 50;
+                chomp.Visible = true;
+                counter = 10;
+            }
+
+
+            if (monster0.Left >= Bounds.Width)
+            {
+                monster0.Top = monster0.Top + 100;
+                cookie0.Top = cookie0.Top + 100;
+                cookie1.Top = cookie1.Top + 100;
+                chomp.Top = chomp.Top + 100;
+
+                monster0.Left = 0;
+                cookie0.Left = 0 + monster0.Width + 50;
+                cookie1.Left = 0 + monster0.Width + cookie0.Width + 50;
+            }
+            if (monster0.Bottom >= Bounds.Bottom)
+            {
+                monster0.Top = 0;
+                cookie0.Top = 0;
+                cookie1.Top = 0;
+                chomp.Top = 100;
+            }
+
+        }
+        private void starttimer_Tick(object sender, System.EventArgs e)
+        {
+            starttimer.Stop();
+            intro0.Visible = false;
+            intro1.Visible = false;
+            monster0.Visible = true;
+            cookie0.Visible = true;
+            cookie1.Visible = true;
+            chomp.Visible = false;
+
+
+
+        }
+ 
+    }
+}
